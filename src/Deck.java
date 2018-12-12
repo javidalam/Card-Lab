78e3ad7c74c192ec03b8bd2b5f419b3cea4f40f1
javidalam <jalam6556@bths.edu>
2018-12-12 10:08:10 -0500

    1

--- src/Deck.java
+++ src/Deck.java
@@ -1,2 +1,12 @@
-public class Deck {
+import java.util.ArrayList;
+
+public class Deck
+{
+    private ArrayList<Card> unDealt;
+    private ArrayList<Card> Dealt;
+
+    public Deck(Card unDealt, Card Dealt)
+    {
+
+    }
 }
